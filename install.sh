cc22c0a175264ba9b79af07b0bbec573be0ec9f3
zconlin <zac.conlin@gmail.com>
2025-10-13 10:03:37 -0600

    Set wallpaper

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
 sudo apt update && sudo apt upgrade -y
+sudo apt install open-vm-tools-desktop -y
+
+# Wallpaper
+mv ./files/z-kali-wallpaper.png $HOME/Pictures/z-kali-wallpaper.png
+sudo ln -sf $HOME/Pictures/z-kali-wallpaper.png /usr/share/desktop-base/kali-theme/login/background
+
